1838f25389f22fdd70026d3cb43158ca423e2eef
Guillaume Riou <griou@sat.qc.ca>
2025-09-09 16:39:15 -0400

    private members for sizes + rename

--- Containers/circular_deque.hpp
+++ Containers/circular_deque.hpp
@@ -11,7 +11,7 @@ concept arithmetic = std::is_arithmetic_v<T>;
 
 /**
  * Simple circular deque style structure with push, pop, max value and very simple size tracking.
-   Written as a fixed sized non allocating replacement for std::deque usage in controlgris.
+ * Written as a fixed sized non allocating replacement for std::deque usage in controlgris.
  */
 template <arithmetic T, std::size_t S>
 class circular_deque {
@@ -22,38 +22,44 @@ class circular_deque {
    * Array of the elements
    */
   std::array<T, S> content;
+  std::size_t max_size;
+  int current_size;
 
  public:
 
-  std::size_t size;
-  int current_size;
-
   circular_deque()
-      : size(S),
+      : max_size(S),
         content{},
         head_index{0},
         current_size{0}
   {}
 
+  std::size_t get_max_size() {
+    return max_size;
+  }
+
+  std::size_t get_current_size() {
+    return max_size;
+  }
 
   /**
    * Pop from the circular buffer.
    * You will get "garbage" if you try to pop while current_size == 0
    */
   T pop() {
-    head_index = (head_index - 1) <= 0 ? size - 1 : head_index - 1;
-    current_size = current_size <= 0 ? 0 : current_size - 1;
+    head_index = (head_index - 1) <= 0 ? max_size - 1 : head_index - 1;
+    current_size = current_size - 1 <= 0 ? 0 : current_size - 1;
     return content[head_index];
   }
 
   /**
-   * push a new element. You will overwrite the first element if current_size == size
+   * push a new element. You will overwrite the first element if current_size == max_size
    */
   void push(T elem) {
     content[head_index] = elem;
     head_index += 1;
-    head_index %= size;
-    current_size = (current_size + 1) >= size ? size : current_size + 1;
+    head_index %= max_size;
+    current_size = (current_size + 1) >= max_size ? max_size : current_size + 1;
   }
 
   /**
@@ -61,16 +67,16 @@ class circular_deque {
    * If you call this with current_size == 0 you will get garbage.
    */
   T max() {
-    std::size_t read_idx = head_index - 1 <= 0 ? size - 1 : head_index - 1;
+    std::size_t read_idx = head_index - 1 <= 0 ? max_size - 1 : head_index - 1;
     T max_value{content[read_idx]};
     for (int i = 1; i < current_size; i++) {
-      max_value = std::max(content[(read_idx + i) % size], max_value);
+      max_value = std::max(content[(read_idx + i) % max_size], max_value);
     }
     return max_value;
   }
 
   /**
-   * Just marks size to 0, doesn't actually clear any values.
+   * Just marks current_size to 0, doesn't actually clear any values.
    */
   void clear() {
     current_size = 0;
