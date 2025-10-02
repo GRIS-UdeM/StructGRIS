5f999e5bdb075cded9e96f1b7114bc5fe8f704f5
Guillaume Riou <griou@sat.qc.ca>
2025-10-02 11:42:21 -0400

    remove specific mention of fork union

--- Containers/sg_TaggedAudioBuffer.hpp
+++ Containers/sg_TaggedAudioBuffer.hpp
@@ -119,7 +119,7 @@ public:
     //==============================================================================
     void copyToPhysicalOutput(float * const * outs, int const numOutputs) const
     {
-        // TODO FU: this could probably be parallelized with fork_union
+        // TODO : this could probably be parallelized with fork_union
         for (auto const buffer : mBuffers) {
             auto const outIndex{ buffer.key.template removeOffset<int>() };
             jassert(outIndex >= 0);
